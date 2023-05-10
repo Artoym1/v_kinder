cd93b8d8978e6d80243df594f1330160218d6d86
Artoym1 <132457836+Artoym1@users.noreply.github.com>
2023-05-11 01:47:15 +0300

    Add files via upload

--- data_store.py
+++ data_store.py
@@ -3,80 +3,34 @@ from config import *
 
 conn = psycopg2.connect(host=host, user=user, password=password, database=db_name)
 
-conn.autocommit = True
+
+def create_table(conn):
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS viewed(
+                user_id INTEGER,
+                found_profile_id INTEGER UNIQUE);
+            """)
+        conn.commit()
 
 
-def create_table_users():
-    with conn.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS users(
-                id serial,
-                first_name varchar(50) NOT NULL,
-                last_name varchar(25) NOT NULL,
-                vk_id varchar(20) NOT NULL PRIMARY KEY,
-                vk_link varchar(20));"""
+def to_db(conn, user_id, found_profile_id):
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            INSERT INTO viewed(user_id, found_profile_id)
+            VALUES (%s, %s);
+            """,
+            (user_id, found_profile_id,)
         )
+        conn.commit()
 
 
-def create_table_seen_users():
-    with conn.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS seen_users(
-            id serial,
-            vk_id varchar(50) PRIMARY KEY);"""
-        )
-
-
-def insert_data_users(first_name, last_name, vk_id, vk_link):
-    with conn.cursor() as cursor:
-        cursor.execute(
-            f"""INSERT INTO users (first_name, last_name, vk_id, vk_link) 
-            VALUES ('{first_name}', '{last_name}', '{vk_id}', '{vk_link}');"""
-        )
-
-
-def insert_data_seen_users(vk_id, offset):
-    with conn.cursor() as cursor:
-        cursor.execute(
-            f"""INSERT INTO seen_users (vk_id) 
-            VALUES ('{vk_id}')
-            OFFSET '{offset}';"""
-        )
-
-
-def select(offset):
-    with conn.cursor() as cursor:
-        cursor.execute(
-            f"""SELECT u.first_name,
-                        u.last_name,
-                        u.vk_id,
-                        u.vk_link,
-                        su.vk_id
-                        FROM users AS u
-                        LEFT JOIN seen_users AS su 
-                        ON u.vk_id = su.vk_id
-                        WHERE su.vk_id IS NULL
-                        OFFSET '{offset}';"""
-        )
-        return cursor.fetchone()
-
-
-def drop_users():
-   with conn.cursor() as cursor:
-        cursor.execute(
-            """DROP TABLE IF EXISTS users CASCADE;"""
-        )
-
-
-def drop_seen_users():
-    with conn.cursor() as cursor:
-        cursor.execute(
-            """DROP TABLE  IF EXISTS seen_users CASCADE;"""
-        )
-
-
-def creating_database():
-    drop_users()
-    drop_seen_users()
-    create_table_users()
-    create_table_seen_users()
+def from_db(conn, user_id):
+    with conn.cursor() as cur:
+        cur.execute("""SELECT found_profile_id FROM viewed WHERE user_id = %s;
+                """,
+                (user_id,)
+            )
+        id_list = cur.fetchall()
+    return id_list
